8724a62ecd97cd076547860ba8da559dd93748ec
Tillmann Schwörer <till.schwoerer@gmx.de>
2019-10-12 14:20:14 +0200

    Add plot

--- 1_tidy_data.R
+++ 1_tidy_data.R
@@ -1,35 +1,41 @@
 library(tidyverse)
 
-p0 <- read_csv("data/population_aged_0_4_years_total_number.csv") %>% 
+p0 <- read_csv("raw_data/population_aged_0_4_years_total_number.csv") %>% 
   pivot_longer(cols = -country, names_to = "year", values_to = "population" ) %>%
   mutate(age = "0-4")
 
-p05 <- read_csv("data/population_aged_5_9_years_total_number.csv") %>% 
+p05 <- read_csv("raw_data/population_aged_5_9_years_total_number.csv") %>% 
   pivot_longer(cols = -country, names_to = "year", values_to = "population" ) %>% 
   mutate(age = "05-09")
 
-p10 <- read_csv("data/population_aged_10_14_years_total_number.csv") %>% 
+p10 <- read_csv("raw_data/population_aged_10_14_years_total_number.csv") %>% 
   pivot_longer(cols = -country, names_to = "year", values_to = "population" ) %>% 
   mutate(age = "10-14")
 
-p15 <- read_csv("data/population_aged_15_19_years_total_number.csv") %>% 
+p15 <- read_csv("raw_data/population_aged_15_19_years_total_number.csv") %>% 
   pivot_longer(cols = -country, names_to = "year", values_to = "population" ) %>% 
   mutate(age = "15-19")
 
-p20 <- read_csv("data/population_aged_20_39_years_total_number.csv") %>% 
+p20 <- read_csv("raw_data/population_aged_20_39_years_total_number.csv") %>% 
   pivot_longer(cols = -country, names_to = "year", values_to = "population" ) %>% 
   mutate(age = "20-39")
 
-p40 <- read_csv("data/population_aged_40_59_years_total_number.csv") %>% 
+p40 <- read_csv("raw_data/population_aged_40_59_years_total_number.csv") %>% 
   pivot_longer(cols = -country, names_to = "year", values_to = "population" ) %>% 
   mutate(age = "40-59")
 
-p60 <- read_csv("data/population_aged_60plus_years_total_number.csv") %>% 
+p60 <- read_csv("raw_data/population_aged_60plus_years_total_number.csv") %>% 
   pivot_longer(cols = -country, names_to = "year", values_to = "population" ) %>% 
   mutate(age = "60+")
 
 
 df <- list(p0, p05, p10, p15, p20, p40, p60) %>% bind_rows()
-df <- df %>% drop_na
+df <- df %>% 
+  drop_na %>% 
+  mutate(year = as.integer(year)) %>% 
+  mutate(age = ifelse(age %in% c("0-4","05-09","10-14","15-19"), "0-19", age)) %>%
+  group_by(country, year, age) %>%
+  summarise(population = sum(population)) %>%
+  ungroup
 
 write_rds(df, "gapminder_tidy.Rds")
---
+++ 2_visualise.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+df <- read_rds("gapminder_tidy.Rds")
+
+theme_set(theme_classic())
+
+p <- df %>% 
+  filter(country %in% c("Germany", "Angola", "Japan", "Peru")) %>%
+  ggplot(aes(x = year, y = population, group = age, color = age)) +
+  geom_line() + geom_point() +
+  facet_wrap(~country, scales = "free_y") +
+  scale_colour_brewer(palette = "Set1")
+
+ggsave("plot.png", p)
+
+
+
