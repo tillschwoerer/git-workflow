bccff3b5145c8c3ecad8047e8a546cbb74f2031c
Tillmann Schwörer <till.schwoerer@gmx.de>
2019-10-12 14:44:06 +0200

    Change color palette

--- 2_visualise.R
+++ 2_visualise.R
@@ -4,12 +4,12 @@ df <- read_rds("gapminder_tidy.Rds")
 theme_set(theme_classic())
 
 p <- df %>% 
-  filter(country %in% c("Germany", "Angola", "Japan", "United States")) %>%
+  filter(country %in% c("Germany", "Angola", "Japan", "Peru")) %>%
   ggplot(aes(x = year, y = population, group = age, color = age)) +
   geom_line() + geom_point() +
   facet_wrap(~country, scales = "free_y") +
   scale_colour_brewer(palette = "Set1")
-
+p
 ggsave("plot.png", p)
 
 
