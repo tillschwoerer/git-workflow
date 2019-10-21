d7b5f05311cf005d1763c6f98eab6d9100cfb297
Steinig1337 <felixd@doobe.net>
2019-10-21 15:08:29 +0200

    Japan gegen China getauscht und zweiten Plot eingefügt der nicht funktioniert

--- 2_explore.R
+++ 2_explore.R
@@ -7,12 +7,19 @@ theme_set(theme_classic())
 # Age distribution over time in 4 countries
 
 p <- df %>% 
-  filter(country %in% c("Germany", "United States", "Japan", "Peru")) %>%
+  filter(country %in% c("Germany", "United States", "China", "Peru")) %>%
   ggplot(aes(x = year, y = population, group = age, color = age)) +
   geom_line() + geom_point() +
   facet_wrap(~country, scales = "free_y") +
   scale_colour_brewer(palette = "Set1")
 p
+p1<-df %>%
+  filter(age %>% c("Germany", "China")) %>%
+  ggplot(aes(x = age, y= population, group = country, color=country)) +
+  geom_line() +geom_point() + 
+  facet_wrap(~age, scales = "free_y") +
+  scale_color_brewer(palette = "Set1")
+p1
 ggsave("plot.png", p)
 
 
